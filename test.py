6391c7b53e03138647639eb4ae59f600ac1b6a4f
TPLV <m4xlion@yandex.ru>
2023-05-07 17:21:48 +0300

    ~

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
+page = 1
 elements = 13
-m = elements / 6
-if m > round(m):
-    print(round(m + 1))
-print(m)
+max = elements / 10
+print(max)
+if max > round(max):
+    max = round(max) + 1
+else:
+    max = round(max)
+elem = min(elements - (page - 1) * 10, 10)
+print(page)
+print(max)
+print(elem)
